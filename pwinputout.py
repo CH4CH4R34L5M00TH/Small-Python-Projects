7f336ff914631e7da6d5fdcced2743adf1feffd4
CH4CH4R34L5M00TH <109595233+CH4CH4R34L5M00TH@users.noreply.github.com>
2023-10-23 20:24:21 +0200

    Add files via upload

---
+++ pwinputout.py
@@ -0,0 +1,25 @@
+import random
+
+def generate_password(length):
+    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+def get_user_input(prompt):
+    return input(prompt).strip()
+
+def main():
+    site_name = get_user_input("Enter the password name for the site: ")
+    output_file = get_user_input("Enter the password output file name (e.g., output.txt): ")
+
+    length = 12  # You can adjust the length of the generated password if needed
+    password = generate_password(length)
+
+    with open(output_file, 'w') as file:
+        file.write(f"Site Name: {site_name}\n")
+        file.write(f"Generated Password: {password}\n")
+
+    print(f"Password for '{site_name}' generated and saved in '{output_file}'")
+
+if __name__ == "__main__":
+    main()
